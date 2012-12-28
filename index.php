2dd46dd58ae7965c935b25c1632d4243493124c9
Drew Ewing <drew.zf@gmail.com>
2012-12-28 13:53:34 -0700

    Implementing homepage
    Implementing a homepage & form for easy debugging and heroku integration

--- index.php
+++ index.php
@@ -1,3 +1,76 @@
+<html>
+    <head>
+        <title>Drew's SEO Tool</title>
+    </head>
+    <body>
+        <div id="container">
+            <div id="content">
+                <?php
+                    if(get_request_var('submit')) {
+                        $twitter = get_post_var('twitter');
+                        $google = get_post_var('google');
+                        $facebook = get_post_var('facebook');
+
+                        $post = get_post_var('post');
+
+                        if($google) {
+                            $email = get_post_var('gusername');
+                            $password = get_post_var('gpassword');
+                            social_google_post($email, $password, $post);
+                        }
+                    }
+                ?>
+                <h1>Post to multiple social networks</h1>
+                <form action="index.php?submit" method="post">
+                    <fieldset>
+                        <div>
+                            <label for="twitter">Post to Twitter</label>
+                            <input type="checkbox" name="twitter" value="1" checked="true">
+
+                            <label for="username">Twitter Username</label>
+                            <input type="text" name="tusername">
+
+                            <label for="password">Twitter Password</label>
+                            <input type="password" name="tpassword">
+                        </div>
+                        <div>
+                            <label for="twitter">Post to Google Plus</label>
+                            <input type="checkbox" name="google" value="1" checked="true">
+
+                            <label for="username">Google Plus Username</label>
+                            <input type="text" name="gusername">
+
+                            <label for="password">Google Plus Password</label>
+                            <input type="password" name="gpassword">
+                        </div>
+                        <div>
+                            <label for="twitter">Post to Facebook</label>
+                            <input type="checkbox" name="facebook" value="1" checked="true">
+
+                            <label for="username">Facebook Username</label>
+                            <input type="text" name="username">
+
+                            <label for="password">Facebook Password</label>
+                            <input type="password" name="password">
+                        </div>
+
+                        <label for="post">Post</label>
+                        <textarea name="post" cols="40" rows="9">Post Content</textarea>
+                        <input type="submit" value="Post">
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
 <?php
-    social_google_post('drew.zf@gmail.com', 'see102507', 'Testing 1 2 3');
+
+function get_post_var($var, $default=false) {
+    return isset($_POST[$var]) ? $_POST[$var] : $default;
+}
+
+function get_request_var($var, $default=false) {
+    return isset($_GET[$var]) ? $_GET[$var] : $default;
+}
+
 ?>

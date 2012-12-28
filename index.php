3fa1672b58dd6642b978d2ae11c65bce21602f79
Drew Ewing <drew.zf@gmail.com>
2012-12-28 13:44:10 -0700

    Adding Heroku Support
    Adding index.php for heroku support

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    social_google_post('drew.zf@gmail.com', 'see102507', 'Testing 1 2 3');
+?>
--- social_tool.php
+++ social_tool.php
@@ -2,7 +2,6 @@
 define('MOBILE_USERAGENT', 'Mozilla/4.0 (compatible; MSIE 5.0; S60/3.0 NokiaN73-1/2.0(2.0617.0.0.7) Profile/MIDP-2.0 Configuration/CLDC-1.1)');
 define('PC_USERAGENT', 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1');
 
-social_google_post('drew.zf@gmail.com', 'see102507', 'Testing 1 2 3');
 
 function social_google_post($email, $password, $post) {
     require_once('libraries/GooglePlusTool.php');

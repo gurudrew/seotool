6dfa412ab8b1a6dcf399de2d12ca9b3d7af2d2a6
Drew Ewing <drew.zf@gmail.com>
2012-12-28 14:39:48 -0700

    Fixing bugs

--- libraries/FacebookTool.php
+++ libraries/FacebookTool.php
@@ -10,6 +10,14 @@
             $this->handle = new Facebook($this->config);
             $this->user = $this->handle->getUser();
             $this->token = $this->handle->getAccessToken();
+            if(!$this->user) {
+                $login_url = $this->handle->getLoginUrl(
+                    array(
+                        'scope' => 'publish_stream'
+                    )
+                );
+                echo '<a href="'. $login_url . '">Log in to Facebook</a>';
+            }
         }
         public function post($name, $link, $message, $description, $picture=false, $actions=false) {
             var_dump($this->user);
@@ -24,12 +32,7 @@
             try {
                 return $this->handle->api('/' . $this->user . '/feed', 'post', $post);
             } catch(FacebookApiException $e) {
-                $login_url = $this->handle->getLoginUrl(
-                    array(
-                        'scope' => 'publish_stream'
-                    )
-                );
-                echo '<a href="'. $login_url . '">Log in to Facebook</a>';
+                var_dump($e);
             }
         }
     }
